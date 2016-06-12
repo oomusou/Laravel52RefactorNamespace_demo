ee587bd2a6e0cad4c94c024f6cdfdcd3ab46eb26
oomusou <oomusou@gmail.com>
2016-06-12 16:23:51 +0800

    建立 PostService

---
+++ app/Services/PostService.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Services;
+
+use App\Repositories\PostRepository;
+use Illuminate\Database\Eloquent\Collection;
+
+class PostService
+{
+    /**
+     * @var PostRepository
+     */
+    private $postRepository;
+
+    /**
+     * PostService constructor.
+     * @param PostRepository $postRepository
+     */
+    public function __construct(PostRepository $postRepository)
+    {
+        $this->postRepository = $postRepository;
+    }
+
+
+    /**
+     * @return Collection
+     */
+    public function displayAllPosts() : Collection
+    {
+        return $this->postRepository->getAllPosts();
+    }
+}
