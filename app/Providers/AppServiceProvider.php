31983807ab1c69261858d9c1a0217f3a4e3032e5
oomusou <oomusou@gmail.com>
2016-07-08 22:40:12 +0800

    新增 Collection::macro(), subset()

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Collection;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -23,6 +24,13 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+        Collection::macro('pick', function ($columns) {
+            return collect($this->items)->map(function ($value) use ($columns) {
+                return collect($columns)
+                    ->map(function ($column) use ($value) {
+                        return $value->$column;
+                    })->all();
+            });
+        });
     }
 }
